8c5237bd6c6c512c7fd4363cdb56ccda956e63c8
Lucas Alfeu <lucas.a1feu@gmail.com>
2022-12-07 14:13:46 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# i Wacth
+
+site de filmes
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <h1>Bem vindo ao futuro site de filmes</h1>
+  </React.StrictMode>
+);
