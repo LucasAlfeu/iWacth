6aae53a4e0d0871f9230200ebe197d4829925396
Lucas Alfeu <lucas.a1feu@gmail.com>
2022-12-16 14:12:58 -0300

    Criando o atom useIdState para armazenar o id dos filmes

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -4,25 +4,28 @@ export const useBackdropState = atom<string>({
     key: 'useBackdropState',
     default: ''
 })
+export const useIdState = atom<number>({
+    key: 'useIdState',
+    default: 0
+ })
+export const useOverviewState = atom<string>({
+    key: 'useOverviewState',
+    default: ''
+})
 export const usePosterState = atom<string>({
     key: 'usePosterState',
     default: ''
 })
-
+export const useReleaseDateState = atom<string>({
+    key: 'useReleaseDateState',
+    default: ''
+})
 export const useTitleState = atom<string>({
     key: 'useTitleState',
     default: ''
 })
 
-export const useOverviewState = atom<string>({
-    key: 'useOverviewState',
-    default: ''
-})
 export const useVoteAverageState = atom<string>({
     key: 'useVoteAverageState',
-    default: ''
-})
-export const useReleaseDateState = atom<string>({
-    key: 'useReleaseDateState',
     default: ''
 })
