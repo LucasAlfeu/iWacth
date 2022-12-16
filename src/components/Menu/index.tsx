d4b932ce60fa87d74bb2cce5fac2f1ba15d0a87b
Lucas Alfeu <lucas.a1feu@gmail.com>
2022-12-16 16:28:56 -0300

    Começando o menu mobile

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import styles from './Menu.module.scss'
 
 export default function Menu() {
+
+    const toggleMenu =() => {
+        const nav = document.getElementById('nav')
+        nav?.classList.toggle('active')
+    }
     return (
         <header className={styles.header}>
             <h1 className={styles.header__title}>iWatch</h1>
-            <nav className={styles.header__nav}>
+            <nav className={styles.header__nav} id='nav'>
+                <button className={styles.header__btnMobile} onClick={() => toggleMenu()}>Menu</button>
                 <ul className={styles.header__menu}>
                     <li className={styles.header__item}>Filmes</li>
                     <li className={styles.header__item}>Séries</li>
--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ root.render(
   <React.StrictMode>
     <RecoilRoot>
       <Menu />
+      <Banner />
     </RecoilRoot>
   </React.StrictMode>
 );
