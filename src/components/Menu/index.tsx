9780f98d1711fb3040b3c377f2fcd78294ca82e3
Lucas Alfeu <lucas.a1feu@gmail.com>
2022-12-19 15:48:49 -0300

    Terminando o responsividade do menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,6 +6,8 @@ import { stringify } from "querystring";
 
 export default function Menu() {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const btn = document.getElementById('button') as HTMLElement
+    const exists = document.body.contains(btn)
 
     const toggleMenu = (e: React.TouchEvent<HTMLButtonElement>) => {
         setIsOpen(!isOpen)
@@ -16,6 +18,7 @@ export default function Menu() {
             <h1 className={styles.header__title}>iWatch</h1>
             <nav className={styles.header__nav} id='nav'>
                 <button
+                id="button"
                     className={styles.header__btnMobile}
                     area-label={!isOpen ? "Abrir Menu" : "Fechar Menu"}
                     onTouchStart={(e) => toggleMenu(e)}
@@ -25,7 +28,8 @@ export default function Menu() {
                 >{!isOpen ? <AiOutlineMenu /> : <AiOutlineCloseCircle />}</button>
                 <ul id="menu" role='menu' className={classNames({
                     [styles.disable]: isOpen === false,
-                    [styles.active]: isOpen === true
+                    [styles.active]: isOpen === true,
+                    [styles.menu]: !exists
                 })}>
                     <li className={styles.header__item}>Filmes</li>
                     <li className={styles.header__item}>Séries</li>
