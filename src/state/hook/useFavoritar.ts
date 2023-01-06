64c1470e003089e807d9d33acd4686ffb6b2e272
Lucas Alfeu <lucas.a1feu@gmail.com>
2023-01-06 15:52:33 -0300

    Adicinando os filmes similares na pagina dos detalhes

--- src/state/hook/useFavoritar.ts
+++ src/state/hook/useFavoritar.ts
@@ -1,5 +1,21 @@
-const useFavoritar = () => {
+import { IProgram } from "interface/IProgram"
+import { useSetRecoilState, useRecoilValue } from "recoil"
+import { useFavoriteProgram } from "state/atom"
 
+const useFavoritar = () => {
+    const setFavoriteProgram = useSetRecoilState(useFavoriteProgram)
+    const favoriteProgram = useRecoilValue(useFavoriteProgram)
+
+    const favoritar = (movie: IProgram) => {
+        if (favoriteProgram.findIndex(element => element === movie) === -1) {
+            setFavoriteProgram([...favoriteProgram, movie])
+        } else {
+            const IdMovieDeslike = favoriteProgram.findIndex(element => element === movie)
+            const favoriteMovies = [...favoriteProgram]
+            favoriteMovies.splice(IdMovieDeslike, 1)
+            setFavoriteProgram([...favoriteMovies])
+        }
+    }
 }
 
 export default useFavoritar
